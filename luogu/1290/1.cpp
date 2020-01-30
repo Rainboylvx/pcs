836da428d0ba1548d1be305ece6cb9215bd1a907
rainboy <rainboylvx@qq.com>
2020-01-30 12:04:06 +0800

    update

--- luogu/1290/1.cpp
+++ luogu/1290/1.cpp
@@ -6,16 +6,23 @@ using namespace std;
 int a,b;
 
 bool gcd(int a,int b){
+    //printf("%d %d\n",a,b);
     if( a % b == 0){
         return 1;
     }
-    return !gcd(b, a %b );
+    int mi = min(b, a %b );
+    int ma = max(b, a %b );
+    if( gcd(ma,mi) == 1){ //输了
+        if(a / b > 1) return 1;
+        return 0;
+    }
+    else return 1;
 }
 
 int main(){
     int t;
-    //scanf("%d",&t);
-    //while(t--){
+    scanf("%d",&t);
+    while(t--){
         scanf("%d%d",&a,&b);
         if( a < b) swap(a,b);
         if( gcd(a,b))
@@ -26,7 +33,7 @@ int main(){
             else
                 printf("Ollie wins\n");
         }
-    //}
+    }
 
 
     return 0;
